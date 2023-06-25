173ea22e34a96f38ce40940641b03672ea32c44b
Lauravll <laura.vll@hotmail.com>
2023-06-25 18:00:29 -0300

    Actualizo unidad 4

--- proyecto_final_v1/frontend/src/components/layouts/Footer.js
+++ proyecto_final_v1/frontend/src/components/layouts/Footer.js
@@ -4,8 +4,6 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 const Footer = (props) => {
   return (
-    
-
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top container ml-4">
     <div class="col-md-4 d-flex align-items-center">
       <span class="mb-3 mb-md-0 color-footer">&copy; 2023 - Buscador de Mascotas - Diseñado por Laura Vallejos</span>
--- proyecto_final_v1/frontend/src/components/layouts/Header.js
+++ proyecto_final_v1/frontend/src/components/layouts/Header.js
@@ -1,6 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 const Header = (props) => {
   return (
-    <h1>Hola</h1>
+    <header>
+      <Navbar data-bs-theme="dark" class="navbar navbar-expand-md navbar-dark shadow-sm" style={{ backgroundColor: '#1a0a3e' }}>
+        <Container>
+          <img src="img/logo.png" alt="Buscador de Mascotas" className="rounded-circle" width="55" height="55" />
+          <Navbar.Brand href="#">Buscador de Mascotas</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#perdidos">Perdidos</Nav.Link>
+            <Nav.Link href="#consejos">Consejos</Nav.Link>
+            <Nav.Link href="#novedades">Novedades</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header> 
+
   )
 }
 
