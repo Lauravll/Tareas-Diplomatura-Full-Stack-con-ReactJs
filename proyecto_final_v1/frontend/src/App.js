00370b497028d314e08bd90a69976f5376958777
Lauravll <laura.vll@hotmail.com>
2023-06-25 19:25:56 -0300

    Actualizo Nav.js con NavLink

--- proyecto_final_v1/frontend/src/App.js
+++ proyecto_final_v1/frontend/src/App.js
@@ -4,25 +4,12 @@ import Header from './components/layouts/Header';
 import Nav from './components/layouts/Nav';
 import Footer from './components/layouts/Footer';
 import { Button } from 'react-bootstrap';
-import HomePage from './pages/HomePage';
-import PerdidosPage from './pages/PerdidosPage';
-import ConsejosPage from './pages/ConsejosPage';
-import NovedadesPage from './pages/NovedadesPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Nav></Nav>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/perdidos" element={<PerdidosPage />} />
-          <Route path="/consejos" element={<ConsejosPage />} />
-          <Route path="/novedades" element={<NovedadesPage />} />
-        </Routes>
-      </BrowserRouter>
       <Footer></Footer>
     </div>
   );
--- proyecto_final_v1/frontend/src/components/layouts/Nav.js
+++ proyecto_final_v1/frontend/src/components/layouts/Nav.js
@@ -1,7 +1,44 @@
-const Nav = (props) => {
+import React from 'react';
+import HomePage from '../../pages/HomePage';
+import PerdidosPage from '../../pages/PerdidosPage';
+import ConsejosPage from '../../pages/ConsejosPage';
+import NovedadesPage from '../../pages/NovedadesPage';
+import { NavLink, BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Nav = () => {
   return (
-    <h1></h1>
-  )
-}
+    <BrowserRouter>
+      <NavLink
+        exact
+        to="/"
+        className={({ isActive }) => (isActive ? "active" : "")}
+      >
+      </NavLink>
+      <NavLink
+        to="/perdidos"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >
+        <NavLink
+          to="/consejos"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+        </NavLink>
+      </NavLink>
+      <NavLink
+        to="/novedades"
+        className={({ isActive }) => (isActive ? "active" : "")}
+      >
+      </NavLink>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/perdidos" element={<PerdidosPage />} />
+        <Route path="/consejos" element={<ConsejosPage />} />
+        <Route path="/novedades" element={<NovedadesPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Nav;
