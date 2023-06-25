8024d022466073fd57db9e3f19394a81db075bab
Lauravll <laura.vll@hotmail.com>
2023-06-25 15:22:31 -0300

    Actualizo unidad 4

--- proyecto_final_v1/frontend/src/App.js
+++ proyecto_final_v1/frontend/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 //import './App.css';
 import Header from './components/layouts/Header';
+import Nav from './components/layouts/Nav';
+import Footer from './components/layouts/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Nav></Nav>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ proyecto_final_v1/frontend/src/components/layouts/Nav.js
@@ -0,0 +1,7 @@
+const Nav = (props) => {
+  return (
+    <h1>Nav</h1>
+  )
+}
+
+export default Nav;
