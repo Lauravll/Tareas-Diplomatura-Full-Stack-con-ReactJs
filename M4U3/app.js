c480da0e6b807224c163a56ea1c5ea4c9b8cb786
Lauravll <laura.vll@hotmail.com>
2023-06-25 22:16:41 -0300

    Actualizo modulo 4 ejercicio 3

--- M4U3/app.js
+++ M4U3/app.js
@@ -22,6 +22,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/ruta1', function (req, res) {
+  res.send('Hola soy la pagina de ruta1');
+})
+
+app.get('/ruta2', function (req, res) {
+  res.send('Hola soy la pagina de ruta2');
+})
+
+app.get('/ruta3', function (req, res) {
+  res.send('Hola soy la pagina de ruta3');
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
