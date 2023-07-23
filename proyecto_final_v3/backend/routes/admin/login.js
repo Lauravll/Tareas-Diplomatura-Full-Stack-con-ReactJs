0a2eae49143e0a6135f9229d18f5e952031b43c9
Lauravll <laura.vll@hotmail.com>
2023-07-23 00:18:21 -0300

    Actualizo trabajo final

--- proyecto_final_v3/backend/routes/admin/login.js
+++ proyecto_final_v3/backend/routes/admin/login.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  /* Carpetad entro de views */
   res.render('admin/login', {
     layout: '/admin/layout',
+    title: 'Buscador de Mascotas' 
   });
 });
 
--- proyecto_final_v3/backend/routes/index.js
+++ proyecto_final_v3/backend/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  //con barra porque es un redireccionamiento
+  res.redirect('admin/login');
 });
 
 module.exports = router;
