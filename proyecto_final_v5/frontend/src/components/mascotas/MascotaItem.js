bd9fea46b861672d39d96eb74b547f5e47f10857
Lauravll <laura.vll@hotmail.com>
2023-08-22 15:02:54 -0300

    Actualizo visualizacion mascotas desde react

--- proyecto_final_v5/frontend/src/components/mascotas/MascotaItem.js
+++ proyecto_final_v5/frontend/src/components/mascotas/MascotaItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+function formatDate(date) {
+  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  return new Date(date).toLocaleDateString('es-ES', options);
+}
+
 const MascotaItem = (props) => {
-  const { nombre_mascota_item, especie_item, raza_item, ojos, pelaje_tipo, pelaje_color, tamanio, fecha_perdido, provincia, localidad, nombre_contacto, apellido_contacto, telefono, email, imagen, body} = props;
+  const { nombre_mascota_item, especie_item, raza_item, ojos, pelaje_tipo, pelaje_color, tamanio, fecha_perdido, provincia, localidad, nombre_contacto, apellido_contacto, telefono, email, imagen, body } = props;
+
+  const fechaFormateada = formatDate(fecha_perdido);
 
   return (
         <div className="col mascotas">
@@ -13,9 +20,9 @@ const MascotaItem = (props) => {
             {nombre_mascota_item} es un {especie_item} de raza {raza_item}, sus ojos son {ojos} y su pelaje {pelaje_tipo} y {pelaje_color}.
             <br />
             Es de tamaño {tamanio}.
-            Se perdió el dia {fecha_perdido} en {provincia} - {localidad}. 
+            Se perdió el dia {fechaFormateada} en {provincia} - {localidad}. 
             <br />
-            Si tienes datos por favor contactarse con {nombre_contacto} {apellido_contacto}, teléfono: {telefono}, email: {email}
+            Si tienes datos contactarse con {nombre_contacto} {apellido_contacto}, teléfono: {telefono}, email: {email}
               </p>
               <div className="d-flex justify-content-between align-items-center">
                 <div>
